2b89d5b3c4ac6f009368e81f9a2efd53d8a08945
Abril-S78 <jiffbom1@gmail.com>
2025-11-11 13:59:14 -0500

    Add files via upload

--- LymeProject/display.py
+++ LymeProject/display.py
@@ -1,13 +1,19 @@
 import streamlit as st
-import extractLocations
+#import extractLocations
 from streamlit_folium import st_folium
 import folium
 
 
 st.title("Lyme centered Locations")
 
-st.write(extractLocations.location_frequency)
- 
+#st.write(extractLocations.location_frequency)
+m = folium.Map([43, -100], zoom_start=7)
+
+url = "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/us_states.json"
+
+folium.GeoJson(url).add_to(m)
+st_data = st_folium(m, width=700, height = 500)
+
 
 
 
