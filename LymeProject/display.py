33af50ea9c46c50b186c84df75bfdfe2b2f5cadc
Abril-S78 <jiffbom1@gmail.com>
2025-11-11 13:39:01 -0500

    Add files via upload
    added in streamline folium function

--- LymeProject/display.py
+++ LymeProject/display.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import extractLocations
+from streamlit_folium import st_folium
+import folium
 
 
 st.title("Lyme centered Locations")
+
 st.write(extractLocations.location_frequency)
+ 
+
+
+
